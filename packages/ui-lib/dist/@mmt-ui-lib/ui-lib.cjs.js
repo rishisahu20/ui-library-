755bd2bfeb9c2c820fd7f07c7ae1d161fe57013f
rishisahu20 <rishi.sahu20@gmail.com>
2022-03-12 14:20:09 +0530

    build-folder-added

---
+++ packages/ui-lib/dist/@mmt-ui-lib/ui-lib.cjs.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var uiLibButton = require('@mmt-ui-lib/ui-lib-button');
+var uiLibText = require('@mmt-ui-lib/ui-lib-text');
+
+
+
+Object.defineProperty(exports, 'Button', {
+	enumerable: true,
+	get: function () { return uiLibButton.Button; }
+});
+Object.defineProperty(exports, 'Text', {
+	enumerable: true,
+	get: function () { return uiLibText.Text; }
+});
---
+++ packages/ui-lib/dist/@mmt-ui-lib/ui-lib.esm.js
@@ -0,0 +1,2 @@
+export { Button } from '@mmt-ui-lib/ui-lib-button';
+export { Text } from '@mmt-ui-lib/ui-lib-text';
